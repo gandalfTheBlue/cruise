d62e82cfb44ed6c872d89d1f9243ac8256a75106
andouillette-ys <yangshan0823@outlook.com>
2019-07-05 12:05:45 +0800

    improve header style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.less'
 import logo from '../../assets/logo/logo.svg'
 import avatar from '../../assets/avatar/yaoming_meme.svg'
 
+const userOptions = [
+  {
+    name: 'Profile',
+    icon: 'icon-id-card'
+  },
+  {
+    name: 'Sign Out',
+    icon: 'icon-sign-in'
+  }
+]
+
 const Avatar = () => {
+  const [showOptions, setShowOptions] = useState(false)
   return (
-    <div className='avatar'>
-      <img src={avatar} alt='avatar' />
-      <span className='iconfont icon-angle-down' />
+    <div>
+      <div className='avatar' onClick={() => setShowOptions(!showOptions)}>
+        <img src={avatar} alt='avatar' />
+        <span className='iconfont icon-angle-down' />
+      </div>
+      <ul className='user-option'>
+        {showOptions &&
+          userOptions.map(option => (
+            <li className='user-menu-item' key={option.name}>
+              <span className={`iconfont ${option.icon}`} />
+              <span className='menu-label'>{option.name}</span>
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
@@ -16,6 +39,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='content'>
+        <div />
         <img src={logo} alt='logo' />
         <Avatar />
       </div>
