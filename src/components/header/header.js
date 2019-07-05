bd206eca075dde8db0221efaf861e144c7ff086c
andouillette-ys <yangshan0823@outlook.com>
2019-07-05 11:06:46 +0800

    add avatar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './header.less'
-import logo from '../../logo.svg'
+import logo from '../../assets/logo/logo.svg'
+import avatar from '../../assets/avatar/yaoming_meme.svg'
+
+const Avatar = () => {
+  return (
+    <div className='avatar'>
+      <img src={avatar} alt='avatar' />
+      <span className='iconfont icon-angle-down' />
+    </div>
+  )
+}
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='content'>
         <img src={logo} alt='logo' />
+        <Avatar />
       </div>
     </div>
   )
