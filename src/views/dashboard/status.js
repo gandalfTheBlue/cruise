16907780a3a49572c81ab296a9e0471e21e1b0d8
andouillette-ys <yangshan0823@outlook.com>
2019-07-05 21:40:16 +0800

    improve cards style

--- src/views/dashboard/status.js
+++ src/views/dashboard/status.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import './dashboard.less'
 
+const cards = [
+  {
+    type: 'Building',
+    typeClass: 'building',
+    customClass: 'icon-cog rotating',
+    number: 3
+  },
+  {
+    type: 'Idle',
+    typeClass: 'idle',
+    customClass: 'icon-coffee',
+    number: 5
+  }
+]
+
+const Card = ({ card }) => (
+  <div className='container'>
+    <div className={`card ${card.typeClass}`}>
+      <span className={`iconfont ${card.customClass}`} />
+      <span className='type'>{card.type}</span>
+      <div className='number'>{card.number}</div>
+    </div>
+  </div>
+)
+
 const Status = () => (
   <div className='status-monitor'>
+    {cards.map(card => (
+      <Card card={card} key={card.type} />
+    ))}
     <div className='container'>
       <div className='card building'>
-        <i className='iconfont icon-cog rotating' />
-      </div>
-    </div>
-    <div className='container'>
-      <div className='card idle'>
-        <i className='iconfont icon-cog' />
-      </div>
-    </div>
-    <div className='container'>
-      <div className='card building'>
-        <i className='iconfont icon-cog' />
+        <span className='iconfont icon-cog' />
       </div>
     </div>
   </div>
